6dbd62a66833cfaa764c688c1bb38630b24de458
Richard Gutkowski <richard@ososolution.com>
2017-02-13 12:24:07 +0000

    Initial route53 script

---
+++ Route53/list_dns_domains.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Script finds all the hosted domains in Route53 and show their names and ID's.
+
+usage() {
+  echo "Usage: $0 <aws config profile>"
+  echo " e.g.: $0 aws_production"
+  echo "Make sure your AWS CLI is configured"
+  echo " "
+  exit 1
+}
+
+if [ -z "$1" ];then
+  usage
+fi
+
+AWS_PROFILE=$1
+
+aws route53 list-hosted-zones | jq -r '.HostedZones[] | .Name + ": " + .Id'
